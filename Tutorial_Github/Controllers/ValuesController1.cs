66b5390e314ab2ea887be62fbeaa157257bf256f
Chirag12343 <bhoirc206@gmail.com>
2025-05-14 16:17:52 +0530

    ert

---
+++ Tutorial_Github/Controllers/ValuesController1.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Tutorial_Github.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController1 : ControllerBase
+    {
+    }
+}
